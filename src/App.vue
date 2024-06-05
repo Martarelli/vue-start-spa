33b6a338cd89b9c21952a057cc548c40a6a2a5e8
Martarelli <renanmartarelli@gmail.com>
2024-06-05 11:31:16 -0300

    publish vuejs project using cli

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <navbar
+        :pages="pages"
+        :active-page="activePage"
+        :nav-link-click="(index) => activePage = index"
+    ></navbar>
+
+    <div v-show="false">
+        hide this content
+    </div>
+
+    <page-viewer 
+        :page="pages[activePage]"
+    ></page-viewer>
+</template>
+
+<script>
+import PageViewer from './components/PageViewer.vue'
+import Navbar from './components/Navbar.vue'
+
+export default {
+    components: {
+        Navbar,
+        PageViewer
+    },
+
+    created(){
+        this.getPages();
+    },
+
+    data(){
+        return {
+            activePage: 0,
+            pages: []
+        };
+    },
+
+    methods : {
+        async getPages(){
+            let res = await fetch('pages.json');
+            let data = await res.json();
+
+            this.pages = data;
+        }
+    }
+}
+</script>

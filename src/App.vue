18cd268bcb7c7700c57ce0279689b22948d74798
martarelli <renanmartarelli@youbrtech.com.br>
2024-06-12 12:08:31 -0300

    feat: nesting routes

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,14 @@
 <template>
-    <navbar
-        :pages="pages"
-    ></navbar>
-
+    <navbar></navbar>
     <router-view></router-view>
-    
-    <!-- <div v-show="false">
-        hide this content
-    </div>
-
-    <page-viewer 
-        :page="pages[activePage]"
-    ></page-viewer>
-
-    <create-page
-        @page-created="pageCreated"
-    ></create-page> -->
 </template>
 
 <script>
-import PageViewer from './components/PageViewer.vue';
 import Navbar from './components/Navbar.vue';
-import CreatePage from './components/CreatePage.vue';
-
 
 export default {
     components: {
         Navbar,
-        PageViewer,
-        CreatePage
     },
 
     methods: {
---
+++ src/views/Pages.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="container">
+        <h2 class="pt-3">Page Management</h2>
+        <router-view></router-view>
+    </div>
+</template>

8a7a07379925c343cb41a7a2b7a678d9d075b9b5
martarelli <renanmartarelli@youbrtech.com.br>
2024-06-13 11:41:28 -0300

    feat: accessing props and router functions

---
+++ src/views/PageEdit.vue
@@ -0,0 +1,12 @@
+<template>
+    edit page {{ props.index }}
+    edit page {{ route.params.index }}
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+const route = useRoute();
+
+const props = defineProps(['index']);
+
+</script>
--- src/views/PagesList.vue
+++ src/views/PagesList.vue
@@ -18,6 +18,7 @@
             <tr
                 v-for="(page, index) in $pages.getAllPages()"
                 :key="index"
+                @click="goToPage(index)"
             >
                 <td>{{ page.pageTitle }}</td>
                 <td>{{ page.link.text }}</td>
@@ -29,8 +30,19 @@
 
 <script setup>
 import {inject} from "vue";
+import { useRouter } from "vue-router";
 
 const $pages = inject('$pages');
+const router = useRouter();
 
+function goToPage(index){
+    router.push({path:`pages/${index}/edit`});
+}
 
 </script>
+
+<style scope>
+.table.table-hover tr:hover{
+    cursor:pointer;
+}
+</style>

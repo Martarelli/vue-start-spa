661a8f9d228d82a830119d55a0013f6179b04238
martarelli <renanmartarelli@youbrtech.com.br>
2024-06-17 11:43:22 -0300

    feat: defining computed and watched values

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -47,7 +47,11 @@ export default {
 
         this.$bus.$on('page-updated', () => {
             this.pages = [...this.$pages.getAllPages()];
-        })
+        });
+
+        this.$bus.$on('page-created', () => {
+            this.pages = [...this.$pages.getAllPages()];
+        });
     },
 
     computed: {
